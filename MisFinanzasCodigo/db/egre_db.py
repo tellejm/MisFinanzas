7cf0a4dd22c1174a6be43f39ef744c73009c5418
Walther Seidel <waseidel@gmail.com>
2020-12-12 12:15:22 -0500

    Egresos creados

---
+++ MisFinanzasCodigo/db/egre_db.py
@@ -0,0 +1,61 @@
+from typing import Dict
+from models.egresos import Egresos
+
+
+
+database_egresos = Dict[str,Egresos]
+
+database_egresos = {
+
+    1: Egresos(**{
+        "Idegresos": 1  ,
+        "descripcion":"Arriendo" ,
+        "frecuencia":"mensual"  ,
+        "importe":330.0   ,
+        "fecha_de_vencimiento":"2020-12-01 23:59:59.000000" ,
+        "estado":"realizado" ,
+        "categoría":"Pagos Recurrentes",
+        "fecha_lanzamiento":"2020-11-01 08:59:00.000000" ,
+        "fecha_pago":"2020-12-01 00:00:00.000000",
+        "observaciones":"Bueno" ,
+        "username":"Jhon"
+    }),
+    2: Egresos(**{
+        "Idegresos": 2,
+        "descripcion": "Pago Electricidad",
+        "frecuencia": "mensual",
+        "importe": 75.0,
+        "fecha_de_vencimiento": "2020-12-15 23:59:59.000000",
+        "estado": "realizado",
+        "categoría": "Pagos Recurrentes",
+        "fecha_lanzamiento": "2020-11-01 09:00:00.000000",
+        "fecha_pago": "2020-12-10 00:00:00.000000",
+        "observaciones": "Bueno",
+        "username": "Jhon"
+    }),
+    3: Egresos(**{
+        "Idegresos": 3,
+        "descripcion": "Pago Acueducto",
+        "frecuencia": "mensual",
+        "importe": 45.0,
+        "fecha_de_vencimiento": "2020-12-10 23:59:59.000000",
+        "estado": "realizado",
+        "categoría": "Pagos Recurrentes",
+        "fecha_lanzamiento": "2020-11-01 09:01:00.000000",
+        "fecha_pago": "2020-12-10 00:00:00.000000",
+        "observaciones": "Bueno",
+        "username": "Jhon"
+    }),
+
+}
+
+
+def get_egresos(Idegresos: int):
+    if Idegresos in database_egresos.keys():
+        return database_egresos[Idegresos]
+    else:
+        return None
+
+def set_egresos(egreso_in_db: Egresos):
+    database_egresos[egreso_in_db.Idegresos] = egreso_in_db
+    return egreso_in_db
--- MisFinanzasCodigo/main.py
+++ MisFinanzasCodigo/main.py
@@ -1,12 +1,12 @@
 
 from fastapi import FastAPI
-from models.user import User
-from db.user_db import set_user, database_users
 
+from db.ingre_db import database_ingresos, set_ingresos
+from db.egre_db import database_egresos, set_egresos
+from db.user_db import database_users, set_user
 from models.ingresos import Ingresos
-from db.ingre_db import set_ingresos, database_ingresos
-
-
+from models.user import User
+from models.egresos import Egresos
 api = FastAPI()
 
 @api.post("/users")
@@ -20,3 +20,8 @@ def saveUser(user: User):
 def saveIngresos(ingresos: Ingresos):
     set_ingresos(ingresos)
     return database_ingresos
+
+@api.post("/egresos")
+def saveEgresos(egresos: Egresos):
+    set_egresos(egresos)
+    return database_egresos
